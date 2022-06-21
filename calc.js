9fca541178266761d6a9a64286b6f86709647fbf
Wynietech <105459074+Wynietech@users.noreply.github.com>
2022-06-21 13:40:04 +0100

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,17 @@
+var opr = prompt("Operator:  ")
+var num1 = parseInt( prompt("Number1;  "))
+var num2 = parseInt( prompt("Number2:  "))
+
+if (opr == "+")
+{
+  alert(num1 + num2)
+} else if (opr == "-")
+{
+   alert(num1 - num2)
+} else if (opr == "*")
+{
+   alert(num1 * num2)
+}else if (opr == "/")
+{
+   alert(num1 / num2)
+}
